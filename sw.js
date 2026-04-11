bb067424f1efb956cc938f6211ad466f1ee95866
Mitul Sendilkumar <mitulisavailable@gmail.com>
2026-04-11 16:33:54 -0500

    latest functioning desktop version

--- sw.js
+++ sw.js
@@ -1,26 +1,5 @@
-const CACHE = 'day-v1';
-const ASSETS = [
-  '/focus_app/',
-  '/focus_app/index.html',
-  '/focus_app/manifest.json',
-  '/focus_app/icon-192.png',
-  '/focus_app/icon-512.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+const CACHE = 'wispora-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
